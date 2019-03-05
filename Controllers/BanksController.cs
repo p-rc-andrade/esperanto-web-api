952dbc86f5e0c31cf4e305b0690ee6a9d94ed057
Unknown <josehrosario@gmail.com>
2019-03-05 23:50:27 +0000

    Add .net core web api project

---
+++ Controllers/BanksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace esperanto_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BanksController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "cdg", "bcp" };
+        }
+    }
+}
